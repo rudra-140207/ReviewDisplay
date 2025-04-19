e947021aac0db23f182e4bb3741b091da9df0ca6
RUDRA PRATAP SINGH <rudra.2226en1029@kiet.edu>
2025-04-20 01:21:47 +0530

    Added initial components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './components/Home'
+import Testinomials from './components/Testinomials'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/testinomials" element={<Testinomials/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ Frontend/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import image1 from "../assets/kiet1.png";
+import image2 from "../assets/kiet2.webp";
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  );
+};
+
+export default Home;
